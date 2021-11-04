aa2e8272e693100a4bc1993a19527525dfe65a27
Skava600 <uskava7@gmail.com>
2021-11-04 16:59:05 +0300

    update: db models.

--- TabataTimer/app/src/main/java/com/example/data/AppDatabase.kt
+++ TabataTimer/app/src/main/java/com/example/data/AppDatabase.kt
@@ -1,4 +1,28 @@
-package com.example.tabatatimer.data
+package com.example.data
 
-class AppDatabase {
+import android.content.Context
+import androidx.room.Room
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = [Workout::class, Interval::class], version = 1, exportSchema = false)
+abstract class AppDatabase: RoomDatabase() {
+
+    /**
+     * Singleton pattern to avoid building the database multiple times
+     */
+    companion object {
+        private var instance: AppDatabase? = null
+        fun getInstance(context: Context): AppDatabase {
+            if (instance == null) {
+                instance = Room.databaseBuilder(
+                    context,
+                    AppDatabase::class.java,
+                    "app_db"
+                ).build()
+            }
+
+            return instance as AppDatabase
+        }
+    }
 }
--- TabataTimer/app/src/main/java/com/example/data/Interval.kt
+++ TabataTimer/app/src/main/java/com/example/data/Interval.kt
@@ -1,4 +1,32 @@
 package com.example.data
 
-class Interval {
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
+import java.io.Serializable
+
+@Entity(foreignKeys = [
+    ForeignKey(entity = Workout::class,
+        parentColumns = ["workoutId"],
+        childColumns = ["intervalId"],
+        onDelete = ForeignKey.CASCADE,
+        onUpdate = ForeignKey.CASCADE)])
+data class Interval(
+    @PrimaryKey(autoGenerate=true) val intervalId: Int?,
+    @ColumnInfo(name="i_name") val intervalName: String,
+    @ColumnInfo(name="i_type") val intervalType: String,
+    @ColumnInfo(name="i_time") val intervalTime: Int?,
+    @ColumnInfo(name="i_reps") val intervalReps: Int?,
+    val workoutId: Int
+) : Serializable {
+    /**
+     * Enum representing the three types of intervals
+     */
+    enum class IntervalType(val value: String)
+    {
+        Prepare("Prepare"),
+        REST("REST"),
+        Work("Work")
+    }
 }
